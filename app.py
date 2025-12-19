28d9be5959255f98c1180ef13e8a569d1203526e
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-12-19 17:33:06 +0800

    Add: dataclass Filters to store datarequest parameters together and validate them

--- app.py
+++ app.py
@@ -11,8 +11,50 @@
 from flask import Flask, redirect, render_template, request, jsonify, g
 from datetime import datetime
 from helpers import validate_form_data, now, calculate_pointval
+from dataclasses import dataclass
+from typing import Optional
 import db
 
+@dataclass
+class Filters:
+    work_type: tuple[str, ...] = ('tdl', 'deep', 'shallow')
+    label: tuple[str, ...] = ()
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+
+    _VALID_WORK_TYPES = ('tdl', 'deep', 'shallow')
+    _VALID_LABELS = () #TODO: extract the valid labels from SQL table: labels
+
+    # Validates the parameters
+    def __post_init__(self):
+
+        # Validates work type
+        if not isinstance(self.work_type, tuple):
+            raise TypeError("Invalid work_type type")
+        invalid = set(self.work_type) - self._VALID_WORK_TYPES
+        if invalid: 
+            return ValueError("Invalid work_type value")
+        
+        # Validates labels
+        if not isinstance(self.label, tuple):
+            raise TypeError("Invalid label type")
+        invalid = set(self.label) - self._VALID_LABELS
+        if invalid: 
+            return ValueError("Invalid label value")
+        
+
+        # validate date related parameters
+        if self.start_date is not None:
+            try: 
+                datetime.fromisoformat(self.start_date)
+            except ValueError:
+                raise ValueError("Invalid start_date")
+            
+        if self.end_date is not None:
+            try: 
+                datetime.fromisoformat(self.end_date)
+            except ValueError:
+                raise ValueError("Invalid start_date")
 
 def create_app():
     app = Flask(__name__)
